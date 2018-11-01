d6941dbf974f4bb98537d0115f38a95f22c5ffc8
jazzworth <jazzworth@hotmail.com>
2018-11-01 15:58:54 -0600

    created first calc function using numpy arrays, the distance calc.  created a Class for the calulations, CalculationsClass and created a main.py.  Presently using static arrays to test the numpy cals and funtion calls.  Will finish all calcs using the static arrays and then work on user input and finally sensor input. - JA

--- CalculationClass.py
+++ CalculationClass.py
@@ -1,19 +1,64 @@
 import numpy as np
+import math as math
 
-ht = np.array([216, 414, 612, 801, 990, 1170])  # array of know height at a given time interval (Ti)
-et = np.array([37.3, 45.8, 51.3, 52.3, 51.1, 48.1])  # elevation angle in degrees measurements taken a Ti
+h_t = np.array([216, 414, 612, 801, 990, 1170])  # array of know height at a given time interval (Ti)
+e_t = np.array([37.3, 45.8, 51.3, 52.3, 51.1, 48.1])  # elevation angle in degrees measurements taken a Ti
+az_t = np.array([88.5, 78.1, 64.3, 55.8, 57.0, 60.9])  # azimuth angle as read from north. will be converted to east
 
 
+# this calculates the distance from the theodolite to be used
 def calculate_distance(height, elevation_angle):
     etan = np.tan(elevation_angle * np.pi / 180)  # tangent of all values in et
+    #distance = np.empty_like(height)
     # since arrays are implicit in numpy, this populates the distance array with out an iterating for loop
     distance = height / etan
+   # print(distance)
     return distance
 
 
-def test(x, y):
-    a = x + y
-    return a
+# Finds the length of side x.  Requires changing degrees to radians to use the function
+def polar_to_rectangular_coordinate_transformation_x(distance_t, azimuth_t):
+    x = distance_t * np.sin(np.deg2rad(azimuth_t))
+    return x
+
+
+# finds the length of side y  Requires changing degrees to radians to use the function
+def polar_to_rectangular_coordinate_transformation_y(distance_t, azimuth_t):
+    y = distance_t * np.cos(np.deg2rad(azimuth_t))
+    return y
+
+
+# requires subtracting the next x_t in the array
+def find_delta_x():
+    pass
+
+
+# requries subtracting the next x_t in the array
+def find_delta_y():
+    pass
+
+
+# this is likely to have the (x, y) and will be transformed to polar in the next function
+def mean_wind_speed():
+    pass
+
+
+# the result of the mean wind speed in (x, y) needs transformed to (point, angle}
+def rectangular_to_polar_coordinate_transformation():
+    pass
+
+
+# changes the angle from computer east to north
+def azimuth_east_to_north():
+    pass
+
+
+def test_funtion():
+    x = 284 * math.sin(math.radians(88.5))
+    return x
+
+#calculate_distance(ht, et)
+#azimuth_north_to_east(azt)
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import numpy as np
 import CalculationClass
 
 
 #calls the distance function using arguments from another file. Arguments may be generated from another funtion or DB
-dt = CalculationClass.calculate_distance(CalculationClass.ht, CalculationClass.et)
-print(dt)
+dt = np.around(CalculationClass.calculate_distance(CalculationClass.h_t, CalculationClass.e_t), decimals=0)
+print("d_t = ", dt)
+test = CalculationClass.test_funtion()
+print("test of x calc = ", test)
+x_t = np.around(CalculationClass.polar_to_rectangular_coordinate_transformation_x(dt, CalculationClass.az_t), decimals=0)
+print(x_t)
+y_t = np.around(CalculationClass.polar_to_rectangular_coordinate_transformation_y(dt, CalculationClass.az_t), decimals=0)
+print(y_t)
 
