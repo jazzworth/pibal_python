f827dbdb72d8e5c0ef0b15fa41af591e003bee2c
jazzworth <jazzworth@hotmail.com>
2019-09-04 15:11:08 -0600

    adding test framework

--- ClassTest.py
+++ ClassTest.py
@@ -9,12 +9,13 @@ class Pibal:
 
     def get_radius_of_pibal(self):
         self.radius = (float(input("what is the diameter of the pibal? "))) / 2 *.0254
-        print('The radius is ' + str(self.radius) + ' meters.')
+        return self.radius
 
 
 def main():
     pibal_object = Pibal()
     pibal_object_radius = pibal_object.get_radius_of_pibal()
+    print('The radius is ' + str(pibal_object_radius) + ' meters.')
 
 if __name__ == "__main__":
     main()
